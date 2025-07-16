7edf9df9b7d1fe8ebc634e9579aa11c3a49c2432
George Gagnon <george@pggagnon.com>
2025-07-16 12:01:21 -0400

    remove unneeded md files

--- update.sh
+++ update.sh
@@ -1,15 +1,28 @@
+#!/bin/sh 
+
 # run updates
 
 brew update
 
+# run all formula
+sh ./brew_formula.sh
+# check for outdated formula
 brew outdated
 brew upgrade
 
+# run all casks
+sh ./brew_casks.sh
+
+# check for outdated casks
 brew cask outdated
 brew cask upgrade
 
 brew cleanup
 
+# run all mac defaults
+sh ./defaults.sh
+
+# update mac app store apps
 mas outdated
 mas upgrade
 
