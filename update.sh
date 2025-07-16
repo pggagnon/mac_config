7d855e5d3de09c772ca8e4a525d8d5e92ef47fd2
George Gagnon <george@pggagnon.com>
2025-07-16 12:22:51 -0400

    add print statements

--- update.sh
+++ update.sh
@@ -5,12 +5,14 @@
 brew update
 
 # run all formula
+echo "running brew_formula"
 sh ./brew_formula.sh
 # check for outdated formula
 brew outdated
 brew upgrade
 
 # run all casks
+echo "running brew_casks"
 sh ./brew_casks.sh
 
 # check for outdated casks
@@ -20,9 +22,11 @@ brew cask upgrade
 brew cleanup
 
 # run all mac defaults
+echo "running mac defaults"
 sh ./defaults.sh
 
 # update mac app store apps
+echo "running mac app store updates"
 mas outdated
 mas upgrade
 
