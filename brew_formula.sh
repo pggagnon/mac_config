340d63f432aa13cac5c68dcfed35a14f0acfd188
George Gagnon <george@pggagnon.com>
2025-07-26 11:26:38 -0400

    add zshrc to init, config borders

--- brew_formula.sh
+++ brew_formula.sh
@@ -22,3 +22,27 @@ brew install uv
 brew install yt-dlp
 brew install hashicorp/tap/terraform
 brew install starship
+
+# Install borders with config
+brew tap FelixKratz/formulae
+brew install borders
+if [ ! -f ~/.config/borders/bordersrc ]; then
+    cat >> ~/.config/borders/bordersrc<< EOF
+    #!/bin/bash
+
+    options=(
+        style=round
+        width=6.0
+        hidpi=on
+        active_color=0xffe2e2e3
+        inactive_color=0xff414550
+    )
+
+    borders "${options[@]}"
+EOF
+    echo "Borders configuration file created at ~/.config/borders/bordersrc"
+else
+    echo "Borders configuration file already exists at ~/.config/borders/bordersrc"
+fi
+
+brew services start borders
--- init.sh
+++ init.sh
@@ -20,25 +20,33 @@ compaudit | xargs chmod g-w
 
 cp .zsh ~/.zsh
 
-# check that the zshrc file exists and contains the lines needed:
-#
-# export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:${PATH}
-# export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-#
-# autoload -Uz compinit && compinit
-# autoload -U +X bashcompinit && bashcompinit
-#
-# complete -o nospace -C /opt/homebrew/bin/terraform terraform
-#
-# eval "$(starship init zsh)"
-#
-# . "$HOME/.local/bin/env"
-#
-# [[ -f ~/.zsh/completions.zsh ]] && source ~/.zsh/completions.zsh
+if [ ! -f ~/.zshrc ]; then
+  echo "Creating .zshrc file"
+  cat >> ~/.zshrc << EOF
+    export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:${PATH}
+    export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
-# update zsh after changes
+    autoload -Uz compinit && compinit
+    autoload -U +X bashcompinit && bashcompinit
+
+    complete -o nospace -C /opt/homebrew/bin/terraform terraform
+
+    eval "$(starship init zsh)"
+
+    . "$HOME/.local/bin/env"
+
+    [[ -f ~/.zsh/completions.zsh ]] && source ~/.zsh/completions.zsh
+EOF
+  echo ".zshrc file created with necessary lines."
+else
+  echo ".zshrc file already exists, ensure it contains the necessary lines."
+fi
+
+# restart zsh after changes
 exec /bin/zsh
 
 # run all installs
+echo "running run_all.sh"
+bash ./run_all.sh
 echo "running update.sh"
 bash ./update.sh
